068d5ac5acd285fa770d443996c81edad5b156bf
Spider <spider@orpheus.network>
2023-07-29 18:30:06 +0000

    Add 24 bit support

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
-use claxon::metadata::StreamInfo;
 use crc32fast::Hasher;
 use hex;
 use serde::{Deserialize, Serialize};
 use std::cmp::min;
 use std::fmt;
 use std::fs::File;
-use std::io::Read;
 use std::path::Path;
 use walkdir::WalkDir;
 static AUDIOINFO_VERSION: i32 = 0;
@@ -17,6 +15,7 @@ enum ProcessError {
     NonFlacError,
     FlacLoadError(FlacLoadError),
     NoSamplesFound,
+    UnsupportedBitDepth,
 }
 
 #[derive(Debug)]
@@ -24,15 +23,18 @@ enum FlacLoadError {
     FileOpenError(std::io::Error),
     FlacReaderError(claxon::Error),
 }
+
 impl fmt::Display for ProcessError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             ProcessError::FlacLoadError(err) => write!(f, "Flac load error: {}", err),
             ProcessError::NoSamplesFound => write!(f, "Total samples not found"),
             ProcessError::NonFlacError => write!(f, "Non flac file found"),
+            ProcessError::UnsupportedBitDepth => write!(f, "Unsupported bit depth"),
         }
     }
 }
+
 impl fmt::Display for FlacLoadError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
@@ -43,6 +45,7 @@ impl fmt::Display for FlacLoadError {
         }
     }
 }
+
 impl From<FlacLoadError> for ProcessError {
     fn from(err: FlacLoadError) -> Self {
         ProcessError::FlacLoadError(err)
@@ -79,6 +82,7 @@ struct Summary {
     bit_depth: i32,
     channels: i32,
 }
+
 #[derive(Debug, Serialize, Deserialize)]
 struct AudioFile {
     file_name: String,
@@ -92,6 +96,7 @@ struct AudioFile {
     crc32: String,
     md5: String,
 }
+
 impl AudioInfo {
     pub fn generate_audio_info_from_path(path: String) -> String {
         let songs = Self::walk_dir(&path);
@@ -124,6 +129,7 @@ impl AudioInfo {
             })
         {
             if entry.file_type().is_file() && entry.path().extension() == Some("flac".as_ref()) {
+                tracing::debug!("Processing: {:?}", &entry);
                 match Self::process_audio_file(&entry) {
                     Ok(audio_file) => {
                         tracing::debug!("{:?}", audio_file);
@@ -145,19 +151,32 @@ impl AudioInfo {
 
     fn process_audio_file(entry: &walkdir::DirEntry) -> Result<AudioFile, ProcessError> {
         if entry.file_type().is_file() && entry.path().extension() == Some("flac".as_ref()) {
-            let (samples, stream_info) = Self::load_flac(entry.path())?;
-
+            let mut reader = Self::load_flac(entry.path())?;
+            let stream_info = reader.streaminfo();
             let total_samples = stream_info.samples.ok_or(ProcessError::NoSamplesFound)?;
-
-            let crc32_checksum = Self::generate_crc32(samples);
+            let bit_depth = stream_info.bits_per_sample;
+            let crc32_checksum: String = match bit_depth {
+                16 => Self::generate_crc32_16bit(
+                    reader
+                        .samples()
+                        .map(|sample| sample.unwrap_or(0) as i16)
+                        .collect(),
+                ),
+                24 => Self::generate_crc32_24bit(
+                    reader
+                        .samples()
+                        .map(|sample| sample.unwrap_or(0) as i32)
+                        .collect(),
+                ),
+                _ => return Err(ProcessError::UnsupportedBitDepth),
+            };
             let duration = total_samples as f32 / stream_info.sample_rate as f32;
-
             let audio_info = AudioFile {
                 file_name: entry.file_name().to_string_lossy().to_string(),
                 duration: Self::format_duration(duration),
                 total_samples,
                 sample_rate: stream_info.sample_rate,
-                bit_depth: stream_info.bits_per_sample,
+                bit_depth,
                 channels: stream_info.channels,
                 peak_level: 0.0,
                 rms_db_level: 0.0,
@@ -170,7 +189,23 @@ impl AudioInfo {
             Err(ProcessError::NonFlacError)
         }
     }
-    fn generate_crc32(samples: Vec<i16>) -> String {
+
+    fn generate_crc32_24bit(samples: Vec<i32>) -> String {
+        let mut crc32 = Hasher::new();
+
+        for sample in samples {
+            let sample_24bit = ((sample as i32) << 8) >> 8;
+            let bytes = [
+                (sample_24bit & 0xFF) as u8,
+                ((sample_24bit >> 8) & 0xFF) as u8,
+                ((sample_24bit >> 16) & 0xFF) as u8,
+            ];
+            crc32.update(&bytes);
+        }
+        let crc32_hash = crc32.finalize();
+        format!("{:X}", crc32_hash)
+    }
+    fn generate_crc32_16bit(samples: Vec<i16>) -> String {
         let mut crc32 = Hasher::new();
 
         for sample in samples {
@@ -179,6 +214,7 @@ impl AudioInfo {
         let crc32_hash = crc32.finalize();
         format!("{:X}", crc32_hash)
     }
+
     fn add_durations(audio_files: &Vec<AudioFile>) -> String {
         let total_duration_secs: f32 = audio_files
             .iter()
@@ -187,6 +223,7 @@ impl AudioInfo {
 
         Self::format_duration(total_duration_secs)
     }
+
     fn parse_duration(duration: &str) -> f32 {
         let components: Vec<&str> = duration.split(':').collect();
         let hours: f32 = components.get(0).unwrap_or(&"0").parse().unwrap_or(0.0);
@@ -212,18 +249,9 @@ impl AudioInfo {
         )
     }
 
-    fn load_flac(path: &Path) -> Result<(Vec<i16>, StreamInfo), FlacLoadError> {
-        let mut flac_file = File::open(path)?;
-        let mut flac_data = Vec::new();
-        flac_file.read_to_end(&mut flac_data)?;
-
-        // Decode FLAC data to PCM samples
-        let mut reader = claxon::FlacReader::new(&flac_data[..])?;
-        let samples: Vec<i16> = reader
-            .samples()
-            .map(|sample| sample.unwrap_or(0) as i16)
-            .collect();
-
-        Ok((samples, reader.streaminfo()))
+    fn load_flac(path: &Path) -> Result<claxon::FlacReader<File>, FlacLoadError> {
+        let flac_file = File::open(path)?;
+        let reader = claxon::FlacReader::new(flac_file)?;
+        Ok(reader)
     }
 }
